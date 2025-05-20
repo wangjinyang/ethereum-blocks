4b9f3eba7584911060178e3c769a0edd7aa5b84e
Tom <214997474@qq.com>
2025-05-21 06:50:45 +0800

    test: save condition

--- src/mappings/blocks.ts
+++ src/mappings/blocks.ts
@@ -1,15 +1,15 @@
-import {
-    ethereum
-} from "@graphprotocol/graph-ts"
+import { ethereum, BigInt } from "@graphprotocol/graph-ts";
 
-import {
-    Block
-} from "../../generated/schema"
+import { Block } from "../../generated/schema";
+
+const limit = BigInt.fromI64(23510000);
 
 export function handleBlock(block: ethereum.Block): void {
-    let id = block.hash.toHex()
-    let blockEntity = new Block(id);
-    blockEntity.number = block.number;
-    blockEntity.timestamp = block.timestamp;
+  let id = block.hash.toHex();
+  let blockEntity = new Block(id);
+  blockEntity.number = block.number;
+  blockEntity.timestamp = block.timestamp;
+  if (blockEntity.number.gt(limit)) {
     blockEntity.save();
   }
+}
